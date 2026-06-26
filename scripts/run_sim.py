e21d3937ee6df4426cb998f522dd5d924ebb0163
Chris Barrett <barrettr.chris@gmail.com>
2026-06-26 10:55:06 -0600

    add entry script

---
+++ scripts/run_sim.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+from src.lbsim.config import Config
+from src.lbsim.simulator import Simulator
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+sys.path.append(root_dir)
+
+def main(args: list[str]) -> int:
+    config_path = os.path.join(os.getcwd(), "configs", args[1])
+    try: 
+        cfg = Config.from_json(config_path)
+        sim = Simulator(cfg)
+        stats = sim.run()
+        stats.write_csv()
+        return 0
+    except Exception as e:
+        print(f"an unexpected error occured: {e}")
+    
+if __name__ == "__main__":
+    raise SystemExit(main(sys.argv))
